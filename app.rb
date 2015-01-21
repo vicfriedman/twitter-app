4ccc37f842fd879b37fe029e8c61a4cef3b4a05e
vicfriedman <victoria.m.friedman@gmail.com>
2015-01-21 17:17:25 -0500

    Testing

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'bundler' #require bundler
 Bundler.require #require everything in bundler in gemfile
 require './lib/twitter_nokogiri'
 
-class MyApp < Sinatra::Base
+class MyApp < Sinatra::Application
   get "/" do
 	puts "IT WORKS"
     @tweets = TwitterNokogiri.new
